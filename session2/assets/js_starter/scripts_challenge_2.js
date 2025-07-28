d4bee703c392507312a504886f082fe9a70e25ca
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-07-28 14:34:39 +0800

    feat(js): Complete JavaScript challenge 1 and 2

---
+++ session2/assets/js_starter/scripts_challenge_2.js
@@ -0,0 +1,17 @@
+// Challenge 2 Starter
+// Initialise the arrays
+let array1 = [1, 2, 3, 2];
+let array2 = [4, 5, 6, 4];
+
+// Use the spread operator to merge the two arrays
+let mergedArray = [...array1, ...array2]; 
+
+// Use the Set object to remove duplicates
+let uniqueArray = [...new Set(mergedArray)]; 
+
+// Use array destructuring to assign the first three elements to variables
+let [first, second, third] = uniqueArray;
+
+// Set the value of the output field to be the second element of the unique array
+let output = document.getElementById("OutputField");
+output.innerHTML = second;
