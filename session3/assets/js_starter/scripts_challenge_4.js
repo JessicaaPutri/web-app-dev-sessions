cf597c9fc184a7958e8c30a3db8be9cf6f119b92
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-08-06 10:12:32 +0800

    feat: Add session 3 code and challenges

---
+++ session3/assets/js_starter/scripts_challenge_4.js
@@ -0,0 +1,31 @@
+// Get the form element
+let form = document.querySelector("#save-me");
+
+// localStorage prefix
+let prefix = "autosave_";
+
+/**
+ * Handle input events
+ * @param  {Event} event The event object
+ */
+function inputHandler(event) {
+  //
+}
+
+/**
+ * Clear all of the saved fields from storage
+ */
+function clearStorage() {
+  //
+}
+
+/**
+ * Load saved data from localStorage
+ */
+function loadSaved() {
+  //
+}
+
+// Load saved data from localStorage
+
+// Listen for DOM events
