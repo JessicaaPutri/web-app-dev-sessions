530667794cb7f1c16171f98ad141318accc6cf25
Jessica Aprilia Putri <jessicaputri2005@icloud.com>
2025-08-25 12:17:51 +0800

    add session 6 code, complete challenges 1 and 2

---
+++ session6/imperative/vanilla_js/index.js
@@ -0,0 +1,66 @@
+// https://react.dev/learn/reacting-to-input-with-state#how-declarative-ui-compares-to-imperative
+async function handleFormSubmit(event) {
+  event.preventDefault();
+  disable(textarea);
+  disable(button);
+  show(loadingMessage);
+  hide(errorMessage);
+  try {
+    await submitForm(textarea.value);
+    show(successMessage);
+    hide(form);
+  } catch (error) {
+    show(errorMessage);
+    errorMessage.textContent = error.message;
+  } finally {
+    hide(loadingMessage);
+    enable(textarea);
+    enable(button);
+  }
+}
+
+function handleTextareaChange() {
+  if (textarea.value.length === 0) {
+    disable(button);
+  } else {
+    enable(button);
+  }
+}
+
+function hide(element) {
+  element.style.display = "none";
+}
+
+function show(element) {
+  element.style.display = "";
+}
+
+function enable(element) {
+  element.disabled = false;
+}
+
+function disable(element) {
+  element.disabled = true;
+}
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer.toLowerCase() === "istanbul") {
+        resolve();
+      } else {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      }
+    }, 1500);
+  });
+}
+
+let form = document.getElementById("form");
+let textarea = document.getElementById("textarea");
+let button = document.getElementById("button");
+let loadingMessage = document.getElementById("loading");
+let errorMessage = document.getElementById("error");
+let successMessage = document.getElementById("success");
+form.onsubmit = handleFormSubmit;
+textarea.oninput = handleTextareaChange;
---
+++ session6/intro_to_state/js_starter/scripts_challenge_1.js
@@ -0,0 +1,25 @@
+// Challenge - Stage 1
+let addBook = function(book){
+// If there's no book to do, do nothing
+// data
+if(!book || book.length < 1) return;
+
+// Get the list
+// Create a new list item
+let list = document.getElementById('list');
+
+// template
+let listItem = document.createElement('li');
+// Append the item to the list
+listItem.textContent = book;
+// render
+list.appendChild(listItem);
+};
+
+let book = "Harry Potter IV";
+let book1 = "Harry Potter V";
+
+addBook(book);
+addBook(book1);
+
+
